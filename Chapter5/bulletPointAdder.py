036d4c5967512d8d0916b6ebd0c10383b9880bb1
Thai-Moraes <thaissa.linguika@gmail.com>
2023-08-30 11:44:35 -0300

    Exercícios adicionados

---
+++ Chapter5/bulletPointAdder.py
@@ -0,0 +1,8 @@
+#! python3
+# bulletPointAdder.py - Adds Wikipedia bullet points to the start
+# of each line of text on the clipboard.
+
+import pyperclip
+text = pyperclip.paste()
+
+pyperclip.copy(text)
