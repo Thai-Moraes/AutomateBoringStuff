7abc82d56fb690ef827ecf0e7602cf5d2f71b6a8
Thai-Moraes <thaissa.linguika@gmail.com>
2023-08-30 11:46:40 -0300

    Exercícios adicionados

--- Chapter5/bulletPointAdder.py
+++ Chapter5/bulletPointAdder.py
@@ -5,4 +5,10 @@
 import pyperclip
 text = pyperclip.paste()
 
+lines = text.split('\n')
+for i in range(len(lines)):    # loop through all indexes in the "lines" list
+    lines[i] = '* ' + lines[i] # add star to each string in "lines" list
+text = '\n'.join(lines)
+
 pyperclip.copy(text)
+pyperclip.paste()
